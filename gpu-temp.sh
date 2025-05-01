64914f654b651cf368e9f54776f796b0ef910a65
CyberSolid-C0DE <MeineZweitHand@gmail.com>
2025-05-01 11:28:48 +0200

    Update gpu-temp.sh

--- gpu-temp.sh
+++ gpu-temp.sh
@@ -1,5 +1,5 @@
 temp="$(nvidia-smi -q -d temperature | awk 'NR==11{print $5}')"
 temp1=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000)) 
 
-echo  "GPU $temp°C " "CPU $temp1°C "
+echo  "CPU $temp1°C " "GPU $temp°C "
 
