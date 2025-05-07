d466f90a1d87cfc8cb681653aec92a34c9ac168a
CyberSolid-C0DE <MeineZweitHand@gmail.com>
2025-05-07 19:28:17 +0200

    Update allinone.sh

--- WaybarScripts/allinone.sh
+++ WaybarScripts/allinone.sh
@@ -4,8 +4,9 @@
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%.0f", 100 - $1}')
 cpu_usage=$(printf "%.0f" "$cpu_usage")
 
-# CPU-Temperatur
-cpu_temp=$(sensors | grep -m 1 -E 'Package id 0|Tdie|temp1' | awk '{print $2}' | tr -d '+°C')
+# CPU-Tempe
+# ratur
+cpu_temp=$(sensors | grep -m 1 'Tctl:' | awk '{gsub(/\+/, "", $2); printf("%d°C", $2)}')
 cpu_temp="${cpu_temp:-N/A}"
 
 # GPU-Auslastung & Temperatur
@@ -21,4 +22,4 @@ fi
 mem_usage=$(free | awk '/Mem:/ { printf("%.0f", $3/$2 * 100) }')
 
 # Pango-Markup mit Farben direkt im Echo
-echo "<span foreground='#cba6f7'>  </span> ${cpu_usage}% <span foreground='#f38ba8'></span> ${cpu_temp}°C | <span foreground='#89b4fa'></span> ${gpu_usage}% <span foreground='#f38ba8'></span> ${gpu_temp}°C | <span foreground='#f9e2af'></span> ${mem_usage}%"
+echo "<span foreground='#cba6f7'>  </span> ${cpu_usage}% <span foreground='#f38ba8'></span> ${cpu_temp} | <span foreground='#89b4fa'></span> ${gpu_usage}% <span foreground='#f38ba8'></span> ${gpu_temp}°C | <span foreground='#f9e2af'></span> ${mem_usage}%"
