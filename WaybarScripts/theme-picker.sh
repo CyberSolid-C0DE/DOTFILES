99e6ba65e1d98d724f9d41bc8859193840c8ed1a
CyberSolid-C0DE <MeineZweitHand@gmail.com>
2025-05-29 12:47:36 +0200

    Create theme-picker.sh

---
+++ WaybarScripts/theme-picker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo -en "\x00prompt\x1f  Waybar Themes\n"
+
+# Statische Liste (du kannst auch dynamisch machen, siehe unten)
+THEMES=("chef" "frappe" "latte" "macchiato" "macho")
+
+if [[ -z "$1" ]]; then
+  # Keine Auswahl übergeben – gib Theme-Liste aus (für Rofi)
+  for theme in "${THEMES[@]}"; do
+    echo "$theme"
+  done
+else
+  # Theme wurde übergeben – führe Wechsel aus
+  ~/.config/waybar-scripts/theme-switcher.sh "$1"
+fi
